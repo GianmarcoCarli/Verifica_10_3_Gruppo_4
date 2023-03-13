9295bad678f3172d6a92bb1b39ff7ef16855550c
GianmarcoCarli <120722256+GianmarcoCarli@users.noreply.github.com>
2023-03-13 11:52:35 +0100

    Commit by Carli

---
+++ Saluto_Carli_Gruppo4.py
@@ -0,0 +1,8 @@
+import Saluto_Badescu_Gruppo4
+import Saluto_Lazeb_Gruppo4
+import Saluto_Parisi_Gruppo4
+def saluto_Carli_Gruppo4():
+    saluto = "Saluti da Gianmarco Carli del gruppo 4"
+    print(saluto)
+
+saluto_Carli_Gruppo4()
